f658c241b5ea0ae1423a96419364b1834d9be809
RuffinaMercy <ruffinamercy22@gmail.com>
2025-12-23 12:03:21 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# Medical ChatBot" 
+
+<!-- Clone Repository -->
+git clone (https://github.com/RuffinaMercy/Medical-ChatBot-1.git)
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MEDICAL-CHATBOT-1",
+    version="0.1.0",
+    author="Ruffina",
+    author_email="ruffinamercy22@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
